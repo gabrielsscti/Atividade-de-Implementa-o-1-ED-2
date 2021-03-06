ec89ee8329b3b2fdf48246de3e4d8f1524ca373c
Gabriel <gss.costa@nca.ufma.br>
2021-03-06 15:54:10 -0300

    Finished

--- teste/Main.java
+++ teste/Main.java
@@ -2,7 +2,6 @@ package teste;
 
 import classes.*;
 
-import java.util.Random;
 import java.lang.Math;
 import java.io.FileWriter;
 import java.util.Arrays;
@@ -11,121 +10,136 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		int numExperiments = 6;
-		String[] file_names = {"ItemA_Results.csv", "ItemB_Results.csv", "ItemC_Results.csv"};
+		final int NUM_EXPERIMENTS = 6; // Número de experimentos para criar vetores de tamanho maior
+		final int ROUNDS = 50; // Número de rodadas para cada experimento, será usado para tirar a média
+
+		String[] file_names = {"ItemA_Results.csv", "ItemB_Results.csv", "ItemC_Results.csv"}; // Nome dos arquivos de saída
 		for(int i=0; i<3; i++){
 			FileWriter fw = null;
 			try{
+				// File Writer
 				fw = new FileWriter(file_names[i]);
 				fw.append("Tamanho de Vetor");
 				fw.append(",");
-				fw.append("Tempo de execução(s)");
+				fw.append("Tempo de execução(s)(média de 50 execuções)");
 				fw.append(",");
-				fw.append("Número de operações");
+				fw.append("Número de operações(média de 50 execuções)");
 				fw.append(",");
 				fw.append("Tipo de Implementação MergeSort");
 				fw.append("\n");
 				System.out.println(file_names[i]);
-				for(int j=1; j<=numExperiments; j++){
+
+				// Loop para cada experimento
+				for(int j=1; j<=NUM_EXPERIMENTS; j++){
 					int n = (int) Math.pow(10, j);
-					Item[] a=null, b=null, c=null, d=null, e=null;
-					switch(i){
-						case 0:
-							a = ItemA.generateRandomItem(n);
-							b = ItemA.generateRandomItem(n);
-							c = ItemA.generateRandomItem(n);
-							d = ItemA.generateRandomItem(n);
-							e = ItemA.generateRandomItem(n);
-							break;
-						case 1:
-							a = ItemB.generateRandomItem(n);
-							b = ItemB.generateRandomItem(n);
-							c = ItemB.generateRandomItem(n);
-							d = ItemB.generateRandomItem(n);
-							e = ItemB.generateRandomItem(n);
-							break;
-						case 2:
-							a = ItemC.generateRandomItem(n);
-							b = ItemC.generateRandomItem(n);
-							c = ItemC.generateRandomItem(n);
-							d = ItemC.generateRandomItem(n);
-							e = ItemC.generateRandomItem(n);
-							break;
-					}
+
+					Item[] itens;
+					
 					long startTime=0;
 
 					System.out.printf("Experimento #%d\n", j);
 					System.out.printf("Utilizando %d objetos aleatórios\n", n);
+					double execTime = 0; // Variável que vai acumular o tempo de execução de cada sort para tirar a média
+					long operationCount = 0; // Variável que vai acumular o número de operações parar tirar a média
+					// Loop para cada round
+					for(int k=0; k<ROUNDS; k++){
+						// Gerando itens de diferentes tipos, como pedido no enunciado
+						itens = getItens(i, n);
 
-					System.out.print("Vetor antes: ");
-					printItemArray(a, 3);
-					startTime = System.nanoTime();
-					Comparable[] aTemp = MergeSort.mergeSort(a, 1);
-					double execTime = (System.nanoTime() - startTime)*Math.pow(10, -9);
-					System.out.printf("Tempo de execução do MergeSort simples: %.5fs\n", execTime);
-					a = comparableToItem(aTemp);
+						System.out.printf("Rodada #%d\n", k);
+						System.out.print("Vetor antes: ");
+						printItemArray(itens, 3);
+						startTime = System.nanoTime();
+						Comparable[] aTemp = MergeSort.mergeSort(itens, 1);
+						execTime += (System.nanoTime() - startTime)*Math.pow(10, -9);
+						itens = comparableToItem(aTemp);
 
-					System.out.println("Número de operações executadas: " + MergeSort.contOp);
-					System.out.print("Vetor depois: ");
-					printItemArray(a, 3);
-					insertCSV(fw, n, execTime, MergeSort.contOp, "Clássica");
+						System.out.println("Número de operações executadas: " + MergeSort.contOp);
+						operationCount += MergeSort.contOp;
+						System.out.print("Vetor depois: ");
+						printItemArray(itens, 3);
 
-					System.out.println();
+						System.out.println();
+					}
+					insertCSV(fw, n, execTime/ROUNDS, operationCount/ROUNDS, "Clássica");
+						
+					execTime = 0;
+					operationCount = 0;
+					for(int k=0; k<ROUNDS; k++){
+						itens = getItens(i, n);
+						System.out.printf("Rodada #%d\n", k);
+						System.out.print("Vetor antes: ");
+						printItemArray(itens, 3);
+						startTime = System.nanoTime();
+						Comparable[] bTemp = MergeSort.mergeSort(itens, 2);
+						execTime += (System.nanoTime() - startTime)*Math.pow(10, -9);
+						itens = comparableToItem(bTemp);
+						System.out.println("Número de operações executadas: " + MergeSort.contOp);
+						operationCount += MergeSort.contOp;
+						System.out.print("Vetor depois: ");
+						printItemArray(itens, 3);
+						System.out.println();
+					}
+					insertCSV(fw, n, execTime/ROUNDS, operationCount/ROUNDS, "Merge com Insert Sort");
+
+					execTime = 0;
+					operationCount = 0;
+					for(int k=0; k<ROUNDS; k++){
+						itens = getItens(i, n);
+
+						System.out.printf("Rodada #%d\n", k);
+						System.out.print("Vetor antes: ");
+						printItemArray(itens, 3);
+						startTime = System.nanoTime();
+						Comparable[] cTemp = MergeSort.mergeSort(itens, 3);
+						execTime += (System.nanoTime() - startTime)*Math.pow(10, -9);
+						itens = comparableToItem(cTemp);
+						System.out.println("Número de operações executadas: " + MergeSort.contOp);
+						operationCount += MergeSort.contOp;
+						System.out.print("Vetor depois: ");
+						printItemArray(itens, 3);
+						System.out.println();
+					}
+					insertCSV(fw, n, execTime/ROUNDS, operationCount/ROUNDS, "Merge testando se está ordenado");
+
+					execTime = 0;
+					operationCount = 0;
+					for(int k=0; k<ROUNDS; k++){
+						itens = getItens(i, n);
 					
-					System.out.print("Vetor antes: ");
-					printItemArray(b, 3);
-					startTime = System.nanoTime();
-					Comparable[] bTemp = MergeSort.mergeSort(b, 2);
-					execTime = (System.nanoTime() - startTime)*Math.pow(10, -9);
-					System.out.printf("Tempo de execução do MergeSort com InsertSort: %.5fs\n",execTime);
-					b = comparableToItem(bTemp);
-					System.out.println("Número de operações executadas: " + MergeSort.contOp);
-					System.out.print("Vetor depois: ");
-					printItemArray(b, 3);
-					insertCSV(fw, n, execTime, MergeSort.contOp, "Merge com Insert Sort");
-
-					System.out.println();
-
-					System.out.print("Vetor antes: ");
-					printItemArray(c, 3);
-					startTime = System.nanoTime();
-					Comparable[] cTemp = MergeSort.mergeSort(c, 3);
-					execTime = (System.nanoTime() - startTime)*Math.pow(10, -9);
-					System.out.printf("Tempo de execução do MergeSort testando se os subvetores já estão ordenados: %.5fs\n",execTime);
-					c = comparableToItem(cTemp);
-					System.out.println("Número de operações executadas: " + MergeSort.contOp);
-					System.out.print("Vetor depois: ");
-					printItemArray(c, 3);
-					insertCSV(fw, n, execTime, MergeSort.contOp, "Merge verificando se subvetores estão ordenados");
-
-					System.out.println();
+						System.out.printf("Rodada #%d\n", k);
+						System.out.print("Vetor antes: ");
+						printItemArray(itens, 3);
+						startTime = System.nanoTime();
+						Comparable[] dTemp = MergeSort.mergeSort(itens, 4);
+						execTime += (System.nanoTime() - startTime)*Math.pow(10, -9);
+						itens = comparableToItem(dTemp);
+						System.out.println("Número de operações executadas: " + MergeSort.contOp);
+						operationCount += MergeSort.contOp;
+						System.out.print("Vetor depois: ");
+						printItemArray(itens, 3);
+					}
+					insertCSV(fw, n, execTime/ROUNDS, operationCount/ROUNDS, "Merge sem cópia de vetores");
 					
-					System.out.print("Vetor antes: ");
-					printItemArray(d, 3);
-					startTime = System.nanoTime();
-					Comparable[] dTemp = MergeSort.mergeSort(d, 4);
-					execTime = (System.nanoTime() - startTime)*Math.pow(10, -9);
-					System.out.printf("Tempo de execução do MergeSort sem cópia de vetores: %.5fs\n", execTime);
-					d = comparableToItem(dTemp);
-					System.out.println("Número de operações executadas: " + MergeSort.contOp);
-					System.out.print("Vetor depois: ");
-					printItemArray(d, 3);
-					insertCSV(fw, n, execTime, MergeSort.contOp, "Merge sem cópia de vetores");
+					execTime = 0;
+					operationCount = 0;
+					for(int k=0; k<ROUNDS; k++){
+						itens = getItens(i, n);
 
-					System.out.println("Vetor antes: ");
-					printItemArray(e, 3);
-					startTime = System.nanoTime();
-					Arrays.sort(e);
-					execTime = (System.nanoTime() - startTime)*Math.pow(10, -9);
-					System.out.printf("Tempo de execução do MergeSort sem cópia de vetores: %.5fs\n", execTime);
-					e = comparableToItem(e);
-					System.out.println("Número de operações executadas desconhecida.");
-					System.out.println("Vetor depois: ");
-					printItemArray(e, 3);
-					insertCSV(fw, n, execTime, -1, "Merge da biblioteca java.utils.Arrays");
-
-					fw.flush();
-					System.out.println();
+						System.out.printf("Rodada #%d\n", k);
+						System.out.println("Vetor antes: ");
+						printItemArray(itens, 3);
+						startTime = System.nanoTime();
+						Arrays.sort(itens);
+						execTime += (System.nanoTime() - startTime)*Math.pow(10, -9);
+						itens = comparableToItem(itens);
+						System.out.println("Número de operações executadas desconhecida.");
+						System.out.println("Vetor depois: ");
+						printItemArray(itens, 3);
+						fw.flush();
+						System.out.println();
+					}
+					insertCSV(fw, n, execTime/ROUNDS, (long)-1, "Merge da biblioteca java.utils.Arrays");
 				}
 				fw.flush();
 				fw.close();
@@ -141,7 +155,19 @@ public class Main {
 		}
 	}
 
-	private static void insertCSV(FileWriter fw, Integer tamVetor, Double tempExecucao, Integer numOperacoes, String impMergeSort){
+	private static Item[] getItens(int type, int n){
+		switch(type){
+			case 0:
+				return ItemA.generateRandomItem(n);
+			case 1:
+				return ItemB.generateRandomItem(n);
+			case 2:
+				return ItemC.generateRandomItem(n);
+		}
+		return null;
+	}
+
+	private static void insertCSV(FileWriter fw, Integer tamVetor, Double tempExecucao, Long numOperacoes, String impMergeSort){
 		try{
 			fw.write(tamVetor.toString());
 			fw.write(",");
@@ -176,13 +202,4 @@ public class Main {
 		}
 		System.out.println();
 	}
-	
-	public static void addRandomNumbers(Integer[] list, int n) {
-		Random rand = new Random();
-		rand.setSeed(System.nanoTime());
-		for(int i=0; i<n; i++) 
-			list[i] = rand.nextInt()%(n*5);
-		
-	}
-
 }
